2a04f9ce40b132cef58860c5a3bdfe12bdc8129f
Tom Rajkovich <tnrajkovich@gmail.com>
2020-11-01 17:04:13 -0600

    Added several comments to the trivia app to make code clearer

--- tandem_code_challenge.rb
+++ tandem_code_challenge.rb
@@ -3,13 +3,16 @@ require 'rubygems'
 require 'json'
 require 'tty-prompt'
 
+# First, the question bank is created by parsing the json file.
 json = File.read('Apprentice_TandemFor400_Data.json')
 question_bank = JSON.parse(json)
 
+#The contestant can enter their name for a custom congratulations at the end if they perform well.
 puts "Welcome to Tandem Trivia! Please enter your name below to begin:"
 name = gets.chomp
 system("clear")
 
+#This loop sets up for a response later that will allow the user to keep playing or exit the application after completion of a full round.
 def play_again?
   loop do
     print "Would you like to play again? Y/N "
@@ -27,16 +30,22 @@ def play_again?
 end
 
 begin
+  #Question count is used to keep track of our loop below, not allowing it to exceed 10 questions.
   question_count = 0
+  #The score resets to 0 at the beginning of each round
   score = 0
   num = question_bank.length - 1
+  #The no_dup_questions variable is to ensure we get a random, non-repeating series of questions in each round of the game.
   no_dup_questions = (0..num).to_a.shuffle
 
   prompt = TTY::Prompt.new
 
   while question_count < 10
+    # i is the index that is pulled from our random question number array for reference in the loop below
     i = no_dup_questions.pop
+    # j is the index for the incorrect answers array for each question
     j = 0
+    # rand_answer is an array that ensures we don't get the same order of answers for the questions (if you play multiple rounds, they will likely come out in a different order to keep it interesting)
     rand_answer = []
     while j < question_bank[i]["incorrect"].length
       rand_answer << question_bank[i]["incorrect"][j]
@@ -58,7 +67,7 @@ begin
         puts "Unfortunately #{question_bank[i]["correct"]} is the correct answer."
         question_count += 1
       end
-    sleep(0.75)
+    sleep(0.75) #This was a decision made to clear the question after some time allowed for the user to see if they got the answer correct before clearing the terminal screen.
     system("clear")
   end
 
